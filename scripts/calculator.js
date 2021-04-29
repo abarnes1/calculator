36b7b6248b047cb281c04fdf0068c60b0e259bbe
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-04-28 21:28:34 -0500

    digits show on display

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,4 +1,10 @@
-let displayMain = document.querySelector("#displayMain");
+const displayMain = document.querySelector("#displayMain");
+let num1 = 0;
+let num2 = 0;
+let operation = null;
+
+let firstNum = 0;
+let secondNum = 0;
 
 console.log(displayMain);
 
@@ -6,11 +12,41 @@ function initializeButtonEvents(){
   let buttons = Array.from(document.querySelectorAll("button"));
 
   buttons.forEach(button  => {
-    button.addEventListener('click', (e) => {
-      console.log(e.target.id);
-    });
+    if(button.value){
+      button.addEventListener('click', (e) => {
+        // if(!isNaN(e.target.value)){
+          appendDigit(e.target.value);
+        // }
+      });
+    }
+    // button.addEventListener('click', (e) => {
+    //   if(e.target.value && !isNaN(e.target.value)){
+    //     appendDigit(e.target.value);
+    //   } else {
+    //     console.log(e.target.id);
+    //   }
+    // });
   });
 }
 
-displayMain.textContent = "0";
+function appendDigit(digit){
+  if(operation === null){
+    let temp = num1.toString() + digit.toString();
+    num1 = parseFloat(num1.toString() + digit.toString());
+    updateDisplay();
+  }
+}
+
+function updateDisplay(){
+  displayMain.textContent = num1;
+}
+
+function resetCalculator() {
+  num1 = 0;
+  num2 = 0;
+  operation = null;
+  updateDisplay();
+}
+
+resetCalculator();
 initializeButtonEvents();
