d1c8c458e582354061640abffa0b76761aed0770
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-04-28 20:32:43 -0500

    log button event to console

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,16 @@
-console.log("linked!");
+let displayMain = document.querySelector("#displayMain");
+
+console.log(displayMain);
+
+function initializeButtonEvents(){
+  let buttons = Array.from(document.querySelectorAll("button"));
+
+  buttons.forEach(button  => {
+    button.addEventListener('click', (e) => {
+      console.log(e.target.id);
+    });
+  });
+}
+
+displayMain.textContent = "0";
+initializeButtonEvents();
